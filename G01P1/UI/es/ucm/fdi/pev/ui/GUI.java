6951fd36116afb6c07d5eab3875f5a0df081d3de
AntonioCardona <toniccibz@gmail.com>
2020-02-27 23:54:42 +0100

    Mas avances en GUI, se ve ventana y opciones, queda enlazar con el Algoritmo Genetico

--- G01P1/UI/es/ucm/fdi/pev/ui/GUI.java
+++ G01P1/UI/es/ucm/fdi/pev/ui/GUI.java
@@ -18,6 +18,7 @@ import es.ucm.fdi.pev.ui.ConfigPanel.DoubleOption;
 import es.ucm.fdi.pev.ui.ConfigPanel.InnerOption;
 import es.ucm.fdi.pev.ui.ConfigPanel.IntegerOption;
 import es.ucm.fdi.pev.ui.ConfigPanel.StrategyOption;
+import es.ucm.fdi.pev.ui.Demo.Figura;
 
 import org.math.plot.Plot2DPanel;
 
@@ -95,11 +96,12 @@ public class GUI extends JFrame {
 		// ELEMENTOS
 		// Grafica:  Declaracion
 		// Ojo las variables final no pueden ser modificadas
-		Grafica g1 = new Grafica();
+		Grafica g1 = new Grafica(panelGrafica);
 		// Le asociamos su Plot2Panel a su panel
 		//_marco.setContentPane(_panel);??? esto como encaja? dentro de la grafica?
-		panelGrafica.add(g1.getGrafica());
-		add(panelGrafica, BorderLayout.EAST);
+		//Plot2DPanel p2p = g1.getGrafica();
+		//panelGrafica.add(p2p);
+		add(panelGrafica, BorderLayout.CENTER);
 		
 		///////////////////////////////////////
 		//
@@ -111,7 +113,7 @@ public class GUI extends JFrame {
 		// asocia el panel con la grafica
 		cp.setTarget(g1);
 		// carga los valores del panel en la gráfica
-		cp.initialize();		
+		//cp.initialize();		
 		add(cp, BorderLayout.WEST);
 		
 	}
@@ -120,14 +122,12 @@ public class GUI extends JFrame {
 	// AQUI ESTABLECEMOS LOS PARAMETROS DEL ALGORITMO GENETICO
 	public ConfigPanel<Grafica> creaPanelConfiguracion() {
 		/////////////// NUESTRAS OPCIONES /////////////////
+		String[] gen = new String[] {"Binario", "Real"};
 		String[] funciones = new String[] { "func 1", "f2: Hölder Table", "f3: Schubert", "f4: Michalewicz"};
-		// Configuracion de opciones posibles
-		// Color[] colores = new Color[] { Color.red, Color.blue, Color.green };		
-		// Selectores
-		//    Tipo de Gen
-		//    TipoGen gen;
-		//    Seleccion
-		//    Cruce
+		String[] selectores = new String[] {"Ruleta", "Torneo", "MUE"};
+		String[] cruces = new String[] {"Monopunto", "Uniforme"};
+		// TODO:
+		// TICK para Elitismo BOOLEAN
 		
 		// DECLARACION / INICIALIZACION DEL PANEL
 		ConfigPanel<Grafica> config = new ConfigPanel<Grafica>();
@@ -135,11 +135,20 @@ public class GUI extends JFrame {
 		////////////////////////////////////
 		// AÑADIR ELEMENTOS
 		// se pueden añadir las opciones de forma independiente, o "de seguido"; el resultado es el mismo.
-		config.addOption(new ChoiceOption<Grafica>(	 // -- eleccion de objeto no-configurable
-			    "funciones",						 // etiqueta 
-			    "fitness del individuo", 			 // tooltip
-			    "color",   							 // campo (debe haber un getColor y un setColor)
-			    funciones));                         // elecciones posibles
+		// LOS 3 strings son: 
+		//        "Etiqueta: Lo que se ve en la ventana"
+		//		  "Tooltip: Pista que se muestra al pasar el raton por encima"
+		//		  "Campo: Buscara Getters y Setters con ese nombre p.ej. getFuncion, setFuncion. (O eso entiendo yo que hace)
+		// ESTABLECER VALORES
+		config.addOption(new IntegerOption<Grafica>("Poblacion:","Define cantidad de individuos", "TamPob", 5, 100));
+		config.addOption(new IntegerOption<Grafica>("Generaciones:","Define cantidad de generaciones", "MaxGen", 10, 100));
+		// CHOICE OPTION
+		config.addOption(new ChoiceOption<Grafica>("Gen","Tipo de gen","Gen", gen));
+		config.addOption(new ChoiceOption<Grafica>("Funcion", "fitness del individuo", "Funcion", funciones));                         // elecciones posibles
+		config.addOption(new ChoiceOption<Grafica>("Seleccion","Que tipo de seleccion usar","Seleccion", selectores));
+		config.addOption(new ChoiceOption<Grafica>("Cruces","Tipo de Cruce","Cruce", cruces));
+		// BOOLEAN (Elitismo, Mutación)
+		// WORK TO DO.
 		//////////////////////////////////////
 		config.endOptions();
 		
@@ -152,22 +161,32 @@ public class GUI extends JFrame {
 	
 	/** Grafica **/
 	public static class Grafica {
+		protected Plot2DPanel _grafica;
+		protected JPanel _panel;
+		protected double[] x_plot;
+		// Tendremos 3 líneas, necesitamos 3 ys // PLOT LINE USA DOUBLES
+		protected double[] maxGen_y_plot;
+		protected double[] maxAbs_y_plot;
+		protected double[] genMed_y_plot;
+		
+		// PARAMETROS QUE RECIBE DEL PANEL DE CONFIGURACION
+		// Duda, ¿no habría que hacer un AGenetico para esto? Lo coge con getters y setters??
 		protected int maxGeneraciones;
 		protected int tamPoblacion;
-		protected Plot2DPanel _grafica;
-		//protected JFrame _marco;
-		protected double[] x_plot = new double[maxGeneraciones]; //Empezamos en generación 1! OJO!
-		// Tendremos 3 líneas, necesitamos 3 ys // PLOT LINE USA DOUBLES
-		protected double[] maxGen_y_plot = new double[maxGeneraciones]; // Máximo de la generación
-		protected double[] genMed_y_plot = new double[maxGeneraciones]; // media generación
-		protected double[] maxAbs_y_plot = new double[maxGeneraciones]; // Maximo absoluto
+		protected String gen;
+		protected String funcion;
+		protected String cruce;
+		protected String seleccion;
+		protected Boolean elitismo;
 		
-		// Constructora sin parametros
-		public void grafica() {
+		public Grafica(JPanel panelGrafica) {
 			_grafica = new Plot2DPanel();
+			_panel = panelGrafica;
+			_panel.add(_grafica);
 		}
 		
 		//Constructora, inicializa las variables
+		/*
 		public void grafica(int maxGen, int tamPob) {
 			maxGeneraciones = maxGen;
 			tamPoblacion = tamPob;
@@ -181,12 +200,19 @@ public class GUI extends JFrame {
 				x_plot[i] = i;
 			}
 		}
+		 */
 		
 		// Esto debe ser llamado en caso de haber generado la gráfica
 		// con la constructora sin argumentos
 		// Despues de haber hecho un setTamPob y setMaxGen
 		// Antes de dibujar.
-		public void initXs() {
+		public void init() {
+			x_plot = new double[maxGeneraciones]; //Empezamos en generación 1! OJO!
+			// Tendremos 3 líneas, necesitamos 3 ys // PLOT LINE USA DOUBLES
+			maxGen_y_plot = new double[maxGeneraciones]; // Máximo de la generación
+			maxAbs_y_plot = new double[maxGeneraciones]; // Maximo absoluto
+			genMed_y_plot = new double[maxGeneraciones]; // media generación
+			
 			//Los Xs van establecidos por defecto (0,1,2,3,4,..., MAX_GENERACIONES-1), tam = maxGeneraciones
 			// OJO QUE EMPEZAMOS POR GENERACION 1, guardar los datos en una posición generación-1.
 			for (int i = 0; i < maxGeneraciones; i++) {
@@ -194,18 +220,51 @@ public class GUI extends JFrame {
 			}
 		}
 		
-		//GETTERS Y SETTERS
+		
+		//GETTERS Y SETTERS NORMALES
+		public Plot2DPanel getGrafica() {
+			return _grafica;
+		}
+		//GETTERS Y SETTERS REQUERIDOS POR EL CONTROL PANEL
 		// Si se ha llamado a la gráfica con la constructora vacía
 		// será necesario llamar a setTamPob y setMaxGen 
 		public void setTamPob(int tamPob) {
 			tamPoblacion = tamPob;
 		}
+		public int getTamPob() {
+			return tamPoblacion;
+		}
 		public void setMaxGen(int maxGen) {
 			maxGeneraciones = maxGen;
 		}
-		public Plot2DPanel getGrafica() {
-			return _grafica;
+		public int getMaxGen() {
+			return maxGeneraciones;
 		}
+		public void setSeleccion(String Seleccion) {
+			seleccion = Seleccion;
+		}
+		public void setGen(String Gen) {
+			gen = Gen;
+		}
+		public String getGen() {
+			return gen;
+		}
+		public void setFuncion(String Funcion) {
+			funcion = Funcion;
+		}
+		public String getFuncion() {
+			return funcion;
+		}
+		public String getSeleccion() {
+			return seleccion;
+		}
+		public void setCruce(String Cruce) {
+			cruce = Cruce;
+		}
+		public String getCruce() {
+			return cruce;
+		}
+		
 		
 		//Método de dibujado
 		protected void dibujaGrafica() 
